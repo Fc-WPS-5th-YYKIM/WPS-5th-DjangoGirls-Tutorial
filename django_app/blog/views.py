9214b9b04a49b8d0f1114ca76d082fccfb8f3a9e
yongyeon-kim <assavictory@gmail.com>
2017-05-29 16:44:56 +0900

    Dynamic post list

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    #return HttpResponse('<html><body>Post list</body></html>')
-    return render(request, 'blog/post_list.html')
+    # context 딕셔너리의 키와 view(html)의 {{}} 변수와 매칭하여 데이터를 파싱 한다.
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    context = {
+        'title': 'PostList from post_list view',
+        'posts': posts
+    }
+    return render(request, 'blog/post_list.html', context=context)
